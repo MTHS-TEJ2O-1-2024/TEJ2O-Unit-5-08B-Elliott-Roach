489bcf86074075116f68fd34d2220d0615fa072b
Elliott-Roach <elliott.roach@mths.ca>
2024-10-30 11:24:34 -0400

    code

--- main.ts
+++ main.ts
@@ -2,7 +2,43 @@
  *
  * Created by: Elliott Roach
  * Created on: oct 2024
- * This program ...
+ * This program turns 90 digres when 10 cm or closer
 */
 
-basic.showString('Hello, World!')
+let distanceToObject: number = 0
+
+
+//clearing
+basic.showIcon(IconNames.Happy)
+
+//moving forward until 10 cm away from wall
+input.onButtonPressed(Button.A, function() {
+    while (distanceToObject > 10) {
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+    }
+    if (distanceToObject < 10) {
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
+}
+
+        if (distanceToObject < 10)  ({
+
+            //turning 90 degrees
+           
+    })
+
+})
+
+//finding distance forever
+basic.forever(function() {
+    //finding distance
+    distanceToObject = sonar.ping(
+        DigitalPin.P15,
+        DigitalPin.P14,
+        PingUnit.Centimeters,
+    )
+    basic.showNumber(distanceToObject)
+})
+
+basic.showNumber(distanceToObject)
